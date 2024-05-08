0204e2418884df9ef1604ed97cf9547a40deaf50
Ben Phillips <ben@ben-phillips.net>
2024-05-08 14:38:32 +0100

    Add a setting for the location of incoming emails

--- src/settings.ts
+++ src/settings.ts
@@ -4,11 +4,13 @@ import { App, PluginSettingTab, Setting } from "obsidian";
 export interface ObsidianGTDSettings {
 	inboxFilePath: string;
 	nextActionsFilePath: string;
+	incomingEmailFolderPath: string;
 }
 
 export const DEFAULT_SETTINGS: Partial<ObsidianGTDSettings> = {
 	inboxFilePath: "Inbox.md",
 	nextActionsFilePath: "Next actions.md",
+	incomingEmailFolderPath: "Email inbox",
 };
 
 export class ObsidianGTDSettingsTab extends PluginSettingTab {
@@ -45,6 +47,18 @@ export class ObsidianGTDSettingsTab extends PluginSettingTab {
 					.setValue(this.plugin.settings.nextActionsFilePath)
 					.onChange(async (value) => {
 						this.plugin.settings.nextActionsFilePath = value;
+						await this.plugin.saveSettings();
+					}),
+			);
+		new Setting(containerEl)
+			.setName("Incoming email folder")
+			.setDesc("The folder where incoming email files are stored")
+			.addText((text) =>
+				text
+					.setPlaceholder("path/to/folder")
+					.setValue(this.plugin.settings.incomingEmailFolderPath)
+					.onChange(async (value) => {
+						this.plugin.settings.incomingEmailFolderPath = value;
 						await this.plugin.saveSettings();
 					}),
 			);
