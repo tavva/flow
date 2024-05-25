670662cfaabaf85a5f498fd37ac93bd4a51d3d8e
Ben Phillips <ben@ben-phillips.net>
2024-05-25 16:24:49 +0100

    Remove redundant code

--- src/components/InboxView.svelte
+++ src/components/InboxView.svelte
@@ -13,11 +13,6 @@
 	export let isProcessingComplete: false
 
 	let inputText: string = currentStage === Stage.File ? line : ''
-	$: if (currentStage === Stage.File) {
-		inputText = line
-	} else if (currentStage === Stage.Folder) {
-		inputText = ''
-	}
 
 	function addToNextActions() {
 		if (inputText.trim() !== '') onAddToNextActions(inputText)
