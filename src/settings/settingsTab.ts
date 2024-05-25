c441fd3c5012744e6722cb791bd880e8e3d6a165
Ben Phillips <ben@ben-phillips.net>
2024-05-25 21:19:47 +0100

    Suggest everything

--- src/settings/settingsTab.ts
+++ src/settings/settingsTab.ts
@@ -1,5 +1,7 @@
 import { App, PluginSettingTab, Setting } from 'obsidian'
 import FlowPlugin from '../main'
+import { FolderSuggest } from './suggesters/FolderSuggester'
+import { FileSuggest, FileSuggestMode } from './suggesters/FileSuggester'
 
 export class FlowSettingsTab extends PluginSettingTab {
 	plugin: FlowPlugin
@@ -17,67 +19,67 @@ export class FlowSettingsTab extends PluginSettingTab {
 		new Setting(containerEl)
 			.setName('Inbox File Path')
 			.setDesc('Path to the Inbox file')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter path to Inbox file')
+			.addSearch((cb) => {
+				new FileSuggest(cb.inputEl, this.plugin)
+				cb.setPlaceholder('Example: Inbox.md')
 					.setValue(this.plugin.settings.inboxFilePath)
 					.onChange(async (value) => {
 						this.plugin.settings.inboxFilePath = value
-						await this.plugin.saveSettings()
-					}),
-			)
+						this.plugin.saveSettings()
+					})
+			})
 
 		new Setting(containerEl)
 			.setName('Inbox Folder Path')
 			.setDesc('Path to the Inbox folder')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter path to Inbox folder')
+			.addSearch((cb) => {
+				new FolderSuggest(cb.inputEl)
+				cb.setPlaceholder('Example: Inbox')
 					.setValue(this.plugin.settings.inboxFolderPath)
 					.onChange(async (value) => {
 						this.plugin.settings.inboxFolderPath = value
-						await this.plugin.saveSettings()
-					}),
-			)
+						this.plugin.saveSettings()
+					})
+			})
 
 		new Setting(containerEl)
 			.setName('Next Actions File Path')
 			.setDesc('Path to the Next Actions file')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter path to Next Actions file')
+			.addSearch((cb) => {
+				new FileSuggest(cb.inputEl, this.plugin)
+				cb.setPlaceholder('Example: Next Actions.md')
 					.setValue(this.plugin.settings.nextActionsFilePath)
 					.onChange(async (value) => {
 						this.plugin.settings.nextActionsFilePath = value
 						await this.plugin.saveSettings()
-					}),
-			)
+					})
+			})
 
 		new Setting(containerEl)
 			.setName('New Project Template File Path')
 			.setDesc('Path to the New Project Template File')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter path to New Project Template file')
+			.addSearch((cb) => {
+				new FileSuggest(cb.inputEl, this.plugin)
+				cb.setPlaceholder('Example: templates/Project.md')
 					.setValue(this.plugin.settings.newProjectTemplateFilePath)
 					.onChange(async (value) => {
 						this.plugin.settings.newProjectTemplateFilePath = value
 						await this.plugin.saveSettings()
-					}),
-			)
+					})
+			})
 
 		new Setting(containerEl)
 			.setName('Projects Folder Path')
 			.setDesc('Path to the Projects Folder')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter path to Projects Folder')
+			.addSearch((cb) => {
+				new FolderSuggest(cb.inputEl)
+				cb.setPlaceholder('Example: Projects')
 					.setValue(this.plugin.settings.projectsFolderPath)
 					.onChange(async (value) => {
 						this.plugin.settings.projectsFolderPath = value
-						await this.plugin.saveSettings()
-					}),
-			)
+						this.plugin.saveSettings()
+					})
+			})
 
 		new Setting(containerEl)
 			.setName('Append to task')
