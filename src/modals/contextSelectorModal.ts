fc3a812c5a355cadb726999a688447c2b123da68
Ben Phillips <ben@ben-phillips.net>
2024-05-31 12:06:33 +0100

    Add option for multi-select

--- src/modals/contextSelectorModal.ts
+++ src/modals/contextSelectorModal.ts
@@ -2,17 +2,22 @@ import { Modal } from 'obsidian'
 
 export class ContextSelectorModal extends Modal {
 	private contexts: string[]
-	private onSelect: (string: String) => void
+	private onSelect: (selected: string[] | string) => void
 	private contextContainer: HTMLElement
+	private multiSelect: boolean
+	private selectedContexts: Set<string>
 
 	constructor(
 		app: App,
 		contexts: string[],
-		onSelect: (string: String) => void,
+		onSelect: (selected: string[] | string) => void,
+		multiSelect: boolean = false,
 	) {
 		super(app)
 		this.contexts = contexts
 		this.onSelect = onSelect
+		this.multiSelect = multiSelect
+		this.selectedContexts = new Set()
 	}
 
 	updateContextList = () => {}
@@ -21,7 +26,7 @@ export class ContextSelectorModal extends Modal {
 		const { contentEl } = this
 		contentEl.createEl('h2', { text: 'Select a Context' })
 
-		this.contextContainer.empty()
+		this.contextContainer?.empty()
 		this.contextContainer = contentEl.createDiv()
 		this.contextContainer.addClass('context-container')
 
@@ -30,10 +35,30 @@ export class ContextSelectorModal extends Modal {
 				text: context,
 			})
 			button.onclick = () => {
-				this.onSelect(context)
-				this.close()
+				if (this.multiSelect) {
+					if (this.selectedContexts.has(context)) {
+						this.selectedContexts.delete(context)
+						button.removeClass('selected')
+					} else {
+						this.selectedContexts.add(context)
+						button.addClass('selected')
+					}
+				} else {
+					this.onSelect(context)
+					this.close()
+				}
 			}
 		})
+
+		if (this.multiSelect) {
+			const doneButton = contentEl.createEl('button', {
+				text: 'Done',
+			})
+			doneButton.addEventListener('click', () => {
+				this.onSelect(Array.from(this.selectedContexts))
+				this.close()
+			})
+		}
 	}
 
 	onClose() {
