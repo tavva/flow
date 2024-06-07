4beca578a12d973bf43cd777182b563c287d3201
Ben Phillips <ben@ben-phillips.net>
2024-06-07 10:29:14 +0100

    Wire up the planning view using the new interface

--- src/components/PlanningView.svelte
+++ src/components/PlanningView.svelte
@@ -1,13 +1,33 @@
 <script lang="ts">
-	import type { Task } from '../planning'
+	import { onMount } from 'svelte'
+	import type { Task } from '../tasks'
+	import FlowPlugin from '../main'
 
-	export let tasks: Task[]
+	export let plugin: FlowPlugin
+
+	let plannedTasks: Task[] = []
+
+	onMount(() => {
+		const unsubscribe = plugin.tasks.plannedTasks.subscribe((value) => {
+			plannedTasks = value
+		})
+
+		return () => unsubscribe()
+	})
+
+	async function onClearTasks() {
+		if (confirm('Are you sure you want to clear all tasks?')) {
+			plugin.tasks.clearTasks()
+		}
+	}
 </script>
 
 <div class="flow-planning-view-container">
 	<ul>
-		{#each tasks as task}
+		{#each plannedTasks as task}
 			<li>{task.title}</li>
 		{/each}
 	</ul>
+
+	<button on:click={onClearTasks}>Clear tasks</button>
 </div>
