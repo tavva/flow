c5ff837ae56ac1f3d27c80f80d494b7dc43fefa6
Ben Phillips <ben@ben-phillips.net>
2024-06-18 09:42:20 +0100

    Dynamically generate view sphere commands

--- src/commands.ts
+++ src/commands.ts
@@ -13,19 +13,6 @@ export async function registerCommands(plugin: FlowPlugin) {
 		callback: () => plugin.stateManager.startProcessing(),
 	})
 
-	// TODO: Don't hardcode spheres
-	plugin.addCommand({
-		id: 'view-personal-sphere',
-		name: 'View Personal Sphere',
-		callback: () => plugin.openSphere('personal'),
-	})
-
-	plugin.addCommand({
-		id: 'view-work-sphere',
-		name: 'View Work Sphere',
-		callback: () => plugin.openSphere('work'),
-	})
-
 	plugin.addCommand({
 		id: 'show-planned-actions',
 		name: 'Show planned actions',
@@ -33,4 +20,30 @@ export async function registerCommands(plugin: FlowPlugin) {
 			openPlanningView(plugin)
 		},
 	})
+
+	resetSphereCommands(plugin)
+}
+
+export async function resetSphereCommands(plugin: FlowPlugin) {
+	const commands: { [key: string]: any } = Object.keys(
+		// @ts-ignore
+		plugin.app.commands['commands'],
+	)
+
+	const sphereCommands: string[] = commands.filter(
+		(c: string) => c.startsWith('flow:view-') && c.endsWith('-sphere'),
+	)
+	for (const command of sphereCommands) {
+		delete commands[command]
+	}
+
+	for (let sphere of plugin.settings.spheres) {
+		sphere = sphere.toLowerCase()
+
+		plugin.addCommand({
+			id: `view-${sphere}-sphere`,
+			name: `View ${sphere} sphere`,
+			callback: () => plugin.openSphere(sphere),
+		})
+	}
 }
