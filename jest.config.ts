386167fb1f9901c362187301d4e797afff988e23
Ben Phillips <ben@ben-phillips.net>
2024-07-03 19:44:33 +0100

    Use ESM modules everywhere

--- jest.config.ts
+++ jest.config.ts
@@ -2,8 +2,9 @@ module.exports = {
 	preset: 'ts-jest',
 	testEnvironment: 'node',
 	moduleFileExtensions: ['ts', 'tsx', 'js'],
-	// transform: {
-	// 	'^.+\\.(ts|tsx)$': 'ts-jest',
-	// },
+	extensionsToTreatAsEsm: ['.ts'],
+	transform: {
+		'^.+\\.(ts|tsx)$': ['ts-jest', { isolatedModules: true, useESM: true }],
+	},
 	testMatch: ['**/tests/**/*.test.(ts|tsx)', '**/*.test.(ts|tsx)'],
 }
