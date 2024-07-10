7204cb4018b810b80e6686d333cf76d8ab2ccab6
Ben Phillips <ben@ben-phillips.net>
2024-07-10 17:10:31 +0100

    Add a setup view Svelte component

---
+++ src/components/SetupView.svelte
@@ -0,0 +1,4 @@
+<script lang="ts">
+</script>
+
+<div>We got a setup</div>
--- src/views/setup.ts
+++ src/views/setup.ts
@@ -1,10 +1,13 @@
-import type FlowPlugin from 'main'
 import { ItemView, WorkspaceLeaf } from 'obsidian'
+
+import type FlowPlugin from 'main'
+import SetupComponent from 'components/SetupView.svelte'
 
 export const SETUP_VIEW_TYPE = 'setup-view'
 
 export class SetupView extends ItemView {
 	plugin: FlowPlugin
+	private component!: SetupComponent
 
 	constructor(leaf: WorkspaceLeaf, plugin: FlowPlugin) {
 		super(leaf)
@@ -21,5 +24,20 @@ export class SetupView extends ItemView {
 
 	override getIcon(): string {
 		return 'waves'
+	}
+
+	async onOpen() {
+		this.component = new SetupComponent({
+			target: this.contentEl,
+			props: {
+				plugin: this.plugin,
+			},
+		})
+	}
+
+	async onClose() {
+		if (this.component) {
+			this.component.$destroy()
+		}
 	}
 }
