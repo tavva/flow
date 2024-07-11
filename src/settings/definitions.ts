e82c878bf89f372253ad10008d6f9fa769d16d3a
Ben Phillips <ben@ben-phillips.net>
2024-07-11 17:16:36 +0100

    Move all setting data and methods to declarative object

--- src/settings/definitions.ts
+++ src/settings/definitions.ts
@@ -1,70 +1,253 @@
-import type FlowPlugin from 'main'
+import { Setting, debounce } from 'obsidian'
 
-export const settingsDefinitions = {
+import type FlowPlugin from 'main'
+import { FileSuggest } from 'settings/suggesters/FileSuggester'
+import { FolderSuggest } from 'settings/suggesters/FolderSuggester'
+import { resetSphereCommands } from 'commands'
+
+interface SettingDefinition<T> {
+	defaultValue: T
+	validate: (value: T) => boolean
+	check?: (value: T, plugin: FlowPlugin) => boolean
+	render: (containerEl: HTMLElement, plugin: FlowPlugin) => void
+}
+
+export const rawSettingsDefinitions = {
 	inboxFilesFolderPath: {
 		defaultValue: '/default/path/inboxFiles',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFolderByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Line at a time')
+				.setDesc(
+					'Flow processes all lines in every note in this folder. We allow multiple inbox notes to help prevent syncing issues when the inbox can be populated from many different sources.',
+				)
+				.addSearch((cb) => {
+					new FolderSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: Inbox')
+						.setValue(plugin.settings.inboxFilesFolderPath)
+						.onChange(async (value) => {
+							plugin.settings.inboxFilesFolderPath = value
+							plugin.saveSettings()
+						})
+				})
+		},
 	},
 	inboxFolderPath: {
 		defaultValue: '/default/path/inbox',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFolderByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Note at a time')
+				.setDesc(
+					'Flow processes entire notes one by one in this folder.',
+				)
+				.addSearch((cb) => {
+					new FolderSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: Inbox')
+						.setValue(plugin.settings.inboxFolderPath)
+						.onChange(async (value) => {
+							plugin.settings.inboxFolderPath = value
+							plugin.saveSettings()
+						})
+				})
+		},
 	},
 	nextActionsFilePath: {
 		defaultValue: '/default/path/nextActions',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFileByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Next actions')
+				.setDesc(
+					`This is where Flow will store Next Actions that aren't
+					assigned to a project.`,
+				)
+				.addSearch((cb) => {
+					new FileSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: Next Actions.md')
+						.setValue(plugin.settings.nextActionsFilePath)
+						.onChange(async (value) => {
+							plugin.settings.nextActionsFilePath = value
+							await plugin.saveSettings()
+						})
+				})
+		},
 	},
 	newProjectTemplateFilePath: {
 		defaultValue: '/default/path/newProjectTemplate',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFileByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('New project template')
+				.setDesc('Flow will create new projects using this template.')
+				.addSearch((cb) => {
+					new FileSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: templates/Project.md')
+						.setValue(plugin.settings.newProjectTemplateFilePath)
+						.onChange(async (value) => {
+							plugin.settings.newProjectTemplateFilePath = value
+							await plugin.saveSettings()
+						})
+				})
+		},
 	},
 	projectsFolderPath: {
 		defaultValue: '/default/path/projects',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFolderByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Projects folder')
+				.setDesc('This is where your project notes are stored.')
+				.addSearch((cb) => {
+					new FolderSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: Projects')
+						.setValue(plugin.settings.projectsFolderPath)
+						.onChange(async (value) => {
+							plugin.settings.projectsFolderPath = value
+							plugin.saveSettings()
+						})
+				})
+		},
 	},
 	newPersonTemplateFilePath: {
 		defaultValue: '/default/path/newPersonTemplate',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFileByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('New person template')
+				.setDesc(
+					'Flow will create new person notes using this template.',
+				)
+				.addSearch((cb) => {
+					new FileSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: templates/Person.md')
+						.setValue(plugin.settings.newPersonTemplateFilePath)
+						.onChange(async (value) => {
+							plugin.settings.newPersonTemplateFilePath = value
+							await plugin.saveSettings()
+						})
+				})
+		},
 	},
 	peopleFolderPath: {
 		defaultValue: '/default/path/people',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFolderByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('People folder')
+				.setDesc('This is where your people notes are stored.')
+				.addSearch((cb) => {
+					new FolderSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: People')
+						.setValue(plugin.settings.peopleFolderPath)
+						.onChange(async (value) => {
+							plugin.settings.peopleFolderPath = value
+							plugin.saveSettings()
+						})
+				})
+		},
 	},
 	somedayFilePath: {
 		defaultValue: '/default/path/someday',
 		validate: (value: string) =>
 			typeof value === 'string' && value.length > 0,
-		checkExists: (value: string, plugin: FlowPlugin) =>
+		check: (value: string, plugin: FlowPlugin) =>
 			plugin.app.vault.getFileByPath(value) !== null,
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Someday/Maybe')
+				.setDesc(
+					'Flow will add your someday/maybe drops using this template.',
+				)
+				.addSearch((cb) => {
+					new FileSuggest(plugin, cb.inputEl)
+					cb.setPlaceholder('Example: Someday.md')
+						.setValue(plugin.settings.somedayFilePath)
+						.onChange(async (value) => {
+							plugin.settings.somedayFilePath = value
+							await plugin.saveSettings()
+						})
+				})
+		},
 	},
-	appendTask: {
+	appendTagToTask: {
 		defaultValue: '',
 		validate: (value: string) => typeof value === 'string',
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Append to task')
+				.setDesc(
+					'Flow will append this to all tasks it creates. This is useful if you use the Tasks plugin and have a global task filter set.',
+				)
+				.addText((text) => {
+					text.setPlaceholder('Enter text to append to tasks')
+						.setValue(plugin.settings.appendTagToTask)
+						.onChange(async (value) => {
+							plugin.settings.appendTagToTask = value
+							await plugin.saveSettings()
+						})
+				})
+		},
 	},
 	spheres: {
 		defaultValue: [] as string[],
 		validate: (value: string[]) =>
 			Array.isArray(value) &&
 			value.every((item) => typeof item === 'string'),
+		render: (containerEl: HTMLElement, plugin: FlowPlugin) => {
+			new Setting(containerEl)
+				.setName('Spheres')
+				.setDesc('A comma-separated list of spheres.')
+				.addText((text) =>
+					text
+						.setPlaceholder('Enter spheres')
+						.setValue(plugin.settings.spheres.join(','))
+						.onChange(
+							debounce(async (value) => {
+								if (!value.trim()) {
+									text.setValue(
+										plugin.settings.spheres.join(','),
+									)
+									return
+								}
+
+								plugin.settings.spheres = value
+									.split(',')
+									.map((sphere) => sphere.trim())
+
+								await plugin.saveSettings()
+								await resetSphereCommands(plugin)
+							}, 5000),
+						),
+				)
+		},
 	},
 }
+
+type SettingsDefinitions = {
+	[K in keyof typeof rawSettingsDefinitions]: SettingDefinition<any>
+}
+
+export const settingsDefinitions: SettingsDefinitions = rawSettingsDefinitions
--- src/settings/settingsTab.ts
+++ src/settings/settingsTab.ts
@@ -1,9 +1,7 @@
-import { PluginSettingTab, Setting, debounce } from 'obsidian'
+import { PluginSettingTab, Setting } from 'obsidian'
 import FlowPlugin from 'main'
-import { FolderSuggest } from 'settings/suggesters/FolderSuggester'
-import { FileSuggest } from 'settings/suggesters/FileSuggester'
 
-import { resetSphereCommands } from '../commands'
+import { settingsDefinitions } from './definitions'
 
 export class FlowSettingsTab extends PluginSettingTab {
 	plugin: FlowPlugin
@@ -18,163 +16,30 @@ export class FlowSettingsTab extends PluginSettingTab {
 
 		containerEl.empty()
 
-		new Setting(containerEl)
-			.setName('Append to task')
-			.setDesc(
-				`Flow will append this to all tasks it creates. This is useful
-				if you use the Tasks plugin and have a global task filter set.`,
-			)
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter text to append to tasks')
-					.setValue(this.plugin.settings.appendTask)
-					.onChange(async (value) => {
-						this.plugin.settings.appendTask = value
-						await this.plugin.saveSettings()
-					}),
-			)
-
-		new Setting(containerEl)
-			.setName('Spheres')
-			.setDesc('A comma-separated list of spheres.')
-			.addText((text) =>
-				text
-					.setPlaceholder('Enter spheres')
-					.setValue(this.plugin.settings.spheres.join(','))
-					.onChange(
-						debounce(async (value) => {
-							if (!value.trim()) {
-								text.setValue(
-									this.plugin.settings.spheres.join(','),
-								)
-								return
-							}
-
-							this.plugin.settings.spheres = value
-								.split(',')
-								.map((sphere) => sphere.trim())
-
-							await this.plugin.saveSettings()
-							await resetSphereCommands(this.plugin)
-						}, 5000),
-					),
-			)
+		settingsDefinitions.appendTagToTask.render(containerEl, this.plugin)
+		settingsDefinitions.spheres.render(containerEl, this.plugin)
 
 		new Setting(containerEl).setName('Inbox folders').setHeading()
 
-		new Setting(containerEl)
-			.setName('Line at a time')
-			.setDesc(
-				`Flow processes all lines in every note in this folder. We
-				 allow multiple inbox notes to help prevent syncing issues when
-				 the inbox can be populated from many different sources.`,
-			)
-			.addSearch((cb) => {
-				new FolderSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: Inbox Files')
-					.setValue(this.plugin.settings.inboxFilesFolderPath)
-					.onChange(async (value) => {
-						this.plugin.settings.inboxFilesFolderPath = value
-						this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('Note at a time')
-			.setDesc('Flow processes entire notes one by one in this folder.')
-			.addSearch((cb) => {
-				new FolderSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: Inbox')
-					.setValue(this.plugin.settings.inboxFolderPath)
-					.onChange(async (value) => {
-						this.plugin.settings.inboxFolderPath = value
-						this.plugin.saveSettings()
-					})
-			})
+		settingsDefinitions.inboxFilesFolderPath.render(
+			containerEl,
+			this.plugin,
+		)
+		settingsDefinitions.inboxFolderPath.render(containerEl, this.plugin)
 
 		new Setting(containerEl).setName('Your notes').setHeading()
 
-		new Setting(containerEl)
-			.setName('Next actions')
-			.setDesc(
-				`This is where Flow will store Next Actions that aren't
-				assigned to a project.`,
-			)
-			.addSearch((cb) => {
-				new FileSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: Next Actions.md')
-					.setValue(this.plugin.settings.nextActionsFilePath)
-					.onChange(async (value) => {
-						this.plugin.settings.nextActionsFilePath = value
-						await this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('New project template')
-			.setDesc('Flow will create new projects using this template.')
-			.addSearch((cb) => {
-				new FileSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: templates/Project.md')
-					.setValue(this.plugin.settings.newProjectTemplateFilePath)
-					.onChange(async (value) => {
-						this.plugin.settings.newProjectTemplateFilePath = value
-						await this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('Projects folder')
-			.setDesc('This is where your project notes are stored.')
-			.addSearch((cb) => {
-				new FolderSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: Projects')
-					.setValue(this.plugin.settings.projectsFolderPath)
-					.onChange(async (value) => {
-						this.plugin.settings.projectsFolderPath = value
-						this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('New person template')
-			.setDesc('Flow will create new person notes using this template.')
-			.addSearch((cb) => {
-				new FileSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: templates/Person.md')
-					.setValue(this.plugin.settings.newPersonTemplateFilePath)
-					.onChange(async (value) => {
-						this.plugin.settings.newPersonTemplateFilePath = value
-						await this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('People folder')
-			.setDesc('This is where your people notes are stored.')
-			.addSearch((cb) => {
-				new FolderSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: People')
-					.setValue(this.plugin.settings.peopleFolderPath)
-					.onChange(async (value) => {
-						this.plugin.settings.peopleFolderPath = value
-						this.plugin.saveSettings()
-					})
-			})
-
-		new Setting(containerEl)
-			.setName('Someday/Maybe')
-			.setDesc(
-				'Flow will add your someday/maybe drops using this template.',
-			)
-			.addSearch((cb) => {
-				new FileSuggest(this.plugin, cb.inputEl)
-				cb.setPlaceholder('Example: Someday.md')
-					.setValue(this.plugin.settings.somedayFilePath)
-					.onChange(async (value) => {
-						this.plugin.settings.somedayFilePath = value
-						await this.plugin.saveSettings()
-					})
-			})
+		settingsDefinitions.nextActionsFilePath.render(containerEl, this.plugin)
+		settingsDefinitions.newProjectTemplateFilePath.render(
+			containerEl,
+			this.plugin,
+		)
+		settingsDefinitions.projectsFolderPath.render(containerEl, this.plugin)
+		settingsDefinitions.newPersonTemplateFilePath.render(
+			containerEl,
+			this.plugin,
+		)
+		settingsDefinitions.peopleFolderPath.render(containerEl, this.plugin)
+		settingsDefinitions.somedayFilePath.render(containerEl, this.plugin)
 	}
 }
