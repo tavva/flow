8bc1ce957e8fdab346020758767775fa0e2fae12
Ben Phillips <ben@ben-phillips.net>
2024-07-22 18:31:37 +0100

    Always start processing when we get the view
    This fixes the bug where the processing view showed the completed step,
    then running the command would simply reopen it even if new items had
    been added to inboxes in the meantime. Explicitly starting processing
    fixes this, at the expense of running it (idemptotently) twice when
    opening the view for the first time

--- src/commands.ts
+++ src/commands.ts
@@ -15,6 +15,7 @@ export async function registerCommands(plugin: FlowPlugin) {
 		name: 'Start processing',
 		callback: () => {
 			plugin.stateManager.setupOrGetProcessingView()
+			plugin.stateManager.startProcessing()
 		},
 	})
 
