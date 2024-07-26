4418c20bdfd5f069b1a1ea295acdd8bfe070e189
Ben Phillips <ben@ben-phillips.net>
2024-07-26 13:30:32 +0100

    Sample weekly review content

--- src/components/WeeklyReviewView.svelte
+++ src/components/WeeklyReviewView.svelte
@@ -8,4 +8,35 @@
 
 <div>
 	<h1>Your weekly review</h1>
+	<h2>Your inboxes</h2>
+	<p>You have 3 unprocessed drops in your inboxes</p>
+	<h2>Projects with no next actions</h2>
+	<ul>
+		<li>Blah project name has nothing actionable. (Why is this bad?)</li>
+	</ul>
+	<h2>Projects with too many next actions</h2>
+	<ul>
+		<li>
+			Another project name has 7 next actions. This is possible, but can
+			indicate bad next action management. (Why is this bad?)
+		</li>
+	</ul>
+	<h2>Waiting for actions with no chase date</h2>
+	<ul>
+		<li>
+			Blah project name has 3 waiting for actions with no chase date. (Why
+			is this bad?)
+		</li>
+	</ul>
+	<h2>Projects that haven't been modified in a while</h2>
+	<ul>
+		<li>
+			Blah project name hasn't been modified in 2 weeks. (Why is this
+			bad?)
+		</li>
+		<li>
+			Another project name hasn't been modified in 3 weeks. (Why is this
+			bad?)
+		</li>
+	</ul>
 </div>
