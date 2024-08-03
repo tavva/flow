02f86770e159882f312c01d8cd4b9d928572680c
Ben Phillips <ben@ben-phillips.net>
2024-08-03 15:12:23 +0100

    Refresh export when we load the plugin

--- src/main.ts
+++ src/main.ts
@@ -70,6 +70,9 @@ export default class FlowPlugin extends Plugin {
 			registerCommands(this)
 			this.registerEvents()
 			this.setupWatchers()
+
+			// Run this here, so we refresh if we've synced from another device
+			this.tasks.exportPlannedTasksDebounced()
 		})
 	}
 
